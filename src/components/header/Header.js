384c0cf2aa199b62b5133dd45d88a874248281e8
HOTPOT <tpcmaky@gmail.com>
2024-08-13 12:23:27 +0200

    removed history.back to replace with simple <a> -> depreciated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,15 @@
 import logo from "../../assets/images/logo/LOGO.svg";
 import Nav from "../nav/Nav";
 
+// history.back() sera obsolète à l'avenir, donc nous devrions utiliser une simple balise <a> pour revenir à la page précédente à l'avenir
+
 export default function Header() {
   return (
     <header className="header">
       <figure className="header__fig">
-      <a href="javascript:history.back()">
+
+        <a href="#">
+
       <img className="logo" src={logo} alt="logo de l'agence kasa" />
         </a>
       </figure>
